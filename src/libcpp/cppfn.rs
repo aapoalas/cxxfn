675c8842d6cdd39c2b92efeda4572c953fbdac1a
Aapo Alasuutari <aapo.alasuutari@gmail.com>
2026-06-21 08:55:22 +0300

    Fix libcpp inner name

--- src/libcpp/cppfn.rs
+++ src/libcpp/cppfn.rs
@@ -1,7 +1,7 @@
 use std::marker::PhantomData;
 
 #[repr(C)]
-pub(crate) struct MsvcCppFn<'a, F: 'static + Copy> {
+pub(crate) struct LibCppFn<'a, F: 'static + Copy> {
     data: [u64; 4],
     _lt: PhantomData<&'a ()>,
     _marker: PhantomData<F>,
